b56d4c57775033c29928e836a52b5c6344b4b769
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-03-30 20:48:56 -0400

    some more for plos...  Just need to run the ft_collect at work.

---
+++ le_r_cites.R
@@ -0,0 +1,22 @@
+library(fulltext)
+library(rcrossref)
+library(dplyr)
+le_works <- cr_works(filter = c(issn = "1572-9761", from_pub_date = "1993-08-01"), sample = 10)
+le_clct <- ft_get("10.1371/journal.pone.0102437") %>% ft_collect()
+
+# PLoS example
+le_plos <- ft_search("landscape ecology", limit = 100)
+for(i in le_plos$plos$data$id){
+  ft_get(i) %>% ft_collect()
+}
+
+le_plos_table <- ft_table()
+
+le_plot_r_cite <- le_plos_table %>%
+  filter(grepl("R: A Language and Environment for Statistical Computing", .$text))
+
+# Need to figure out how to get year published easier for cr_
+
+le_plos_table %>%
+  filter(grepl("SAS", .$text))
+
