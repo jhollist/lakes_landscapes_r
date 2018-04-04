c0392b46c163be56c3005b116f1eb38bb8c81666
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-04-04 12:30:22 -0400

    added linux version of render slides.  Decktape woes

---
+++ render_slides_linux.R
@@ -0,0 +1,7 @@
+#system("git checkout gh-pages")
+rmarkdown::render("index.Rmd")
+#system("phantomjs /usr/local/decktape-1.0.0/decktape.js index.html index.pdf")
+system("`npm bin`/decktape remark index.html lakes_landscapes_r.pdf")
+system("git add -A")
+system('git commit -m "fresh render index"')
+system("git push origin master")
